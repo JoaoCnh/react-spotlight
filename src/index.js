ac1149ec858fea97a2c6fdfcd642105559692156
João Cunha <joao.cunha@wiremaze.com>
2018-12-03 14:17:27 +0000

    adding hint text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import styled from "styled-components";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
@@ -7,4 +8,20 @@ import Spotlight from "./Spotlight";
 
 library.add(faSearch);
 
-render(<Spotlight />, document.querySelector("#app"));
+const Kbd = styled.kbd`
+  border: 1px solid #666666;
+  border-radius: 4px;
+  padding: 3px 5px;
+  margin: 2px;
+  color: #444444;
+  text-decoration: none;
+`;
+
+render(
+  <div>
+    To open press <Kbd>ctrl</Kbd>
+    <Kbd>space</Kbd>
+    <Spotlight />
+  </div>,
+  document.querySelector("#app")
+);
